ad2b4ab15dc061f02496dd7a5c29f01c73b37161
Platon172 <107851230+Platon172@users.noreply.github.com>
2023-05-23 18:12:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function Numbers() {
+  for (let i = 3; i <= 13; i++) {
+    console.log(i);
+  }
+}
+
+Numbers();
+
+//2
+
+function Number10() {
+  for (let i = 0; i < 10; i++) {
+    console.log(10);
+  }
+}
+
+Number10();
+
+
+//3
+
+
+function Number1(b1, b2) {
+  if (b1 > b2) {
+    console.log(b1);
+  } else if (b2 > b1) {
+    console.log(b2);
+  } 
+}
+
+Number1(6, 9); 
+Number1(6, 1); 
